1e2e05ecfae49206f10732dc47887784492d8780
unknown <facumarcati99@gmail.com>
2025-08-25 17:10:28 -0300

    agrego funcion eliminar peliculas fav + estilos

--- js/main.js
+++ js/main.js
@@ -318,7 +318,7 @@ function mostrarPeliculasFavoritas() {
             <div class="titulo-star"><h2 class="titulo-pelicula">${
               pelicula.titulo
             }</h2>
-            <div class="container-icon">
+            <div class="container-x-icon">
             <h2><svg class="x-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z"/></svg></h2></div></div>
             <div class="anio-duracion-pelicula"><h2 class="anio-pelicula"> ${
               pelicula.anio ? pelicula.anio : "N/A"
@@ -328,12 +328,30 @@ function mostrarPeliculasFavoritas() {
               pelicula.duracion > 0 ? pelicula.duracion + " min" : "N/A"
             }</h2></div></div>`;
 
+      const iconoEliminar = peliculaElement.querySelectorAll(".x-icon");
+      iconoEliminar.forEach((icono) => {
+        icono.addEventListener("click", () => {
+          const confirma = confirm();
+          if (confirma) {
+            eliminarPeliculaFavorita(pelicula);
+          }
+        });
+      });
+
       peliculaElement.appendChild(generosContainer);
       resultadoPelicula.appendChild(peliculaElement);
     });
   } catch (error) {
     alert("Ocurrió un error al buscar la película: " + error);
   }
+}
+
+function eliminarPeliculaFavorita(pelicula) {
+  peliculasFavoritas = peliculasFavoritas.filter((p) => p.id !== pelicula.id);
+
+  guardarListaPeliculasFavoritas();
+
+  mostrarPeliculasFavoritas();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -372,7 +390,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // TO DO
-// Borrar favorito - funcion para que toques la x y se borre la peli de favs
-// Agregar un boton para volver al header de la pagina
-// Agregar footer - OPCIONAL
-// Modo oscuro/claro - ULTIMO
+// Agregarle UX/UI a las peliculas - sombreado a las cards / hover en generos / iconos en año y duracion
+// Animaciones - agregar animaciones para cuando se agrega o se elimina una peli de favs
+// o cuando se quiere agregar una peli que ya esta en favoritos
+// Click para mas - al hacer click en la pelicula que se abra una pantalla que contenga mas detalles
+// de la pelicula - OPCIONALs
